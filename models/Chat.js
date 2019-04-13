f9119d04b8ec656e338f3ddb9ad307095f81ee16
AlexBulatov <al.ross@yandex.ru>
2019-04-13 17:18:13 +0700

    Added Chat model

---
+++ models/Chat.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Chat = new Schema(
+    {
+        receiver: String,
+        sender: String,
+        text: String,
+        createdAt: {type: Date, default: Date.now }
+    }
+);
+
+module.exports = mongoose.model("messages", Chat);
